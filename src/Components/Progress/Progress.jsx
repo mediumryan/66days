ff94d098ad7e5bf640132e0082b07c08b0b10959
Ryan <straightwind0726@gmail.com>
2023-10-04 17:15:47 +0900

    [Update] Generated Progress Component

--- src/Components/Progress/Progress.jsx
+++ src/Components/Progress/Progress.jsx
@@ -1,58 +1,134 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
+import {
+    completeCountState,
+    completePercentState,
+    dateSubmitted,
+    endDateState,
+    startDateState,
+} from '../../atom';
+import { useForm } from 'react-hook-form';
+import { FaPlus } from 'react-icons/fa';
+import { SlNote } from 'react-icons/sl';
 
 const ProgressWrapper = styled.div`
     position: relative;
-    background-color: green;
+    background-color: var(--bg-200);
+    color: var(--text-200);
+    font-size: var(--font-size-micro);
     width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
-    padding: 24px;
+    padding: var(--padding-double-large);
+    border-radius: 20px;
 `;
 
 const Per = styled.span`
     position: absolute;
-    top: 0%;
-    right: 0;
+    top: 10%;
+    right: 2.5%;
+    color: var(--primary-200);
 `;
 
 const CompleteCount = styled.span``;
 
 const ProgressBarBack = styled.div`
-    background-color: white;
-    width: 50%;
-    height: 10px;
+    background-color: var(--bg-300);
+    border-radius: 8px;
+    width: 100%;
+    height: 25px;
+    overflow: hidden;
+    margin: var(--margin-medium-large) 0;
 `;
 
 const ProgressBar = styled.div`
-    background-color: red;
-    width: 20%;
-    height: 10px;
+    background-color: var(--primary-200);
+    width: ${(props) => props.per}%;
+    height: 100%;
 `;
 
-const ProgressBottom = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
+const DateValue = styled.span`
+    form {
+        display: flex;
+        align-items: center;
+        input {
+            margin: 0 var(--margin-small);
+        }
+    }
 `;
 
-const FailCount = styled.span``;
+const StartEnd = styled.div`
+    position: relative;
+    button {
+        position: absolute;
+        top: 50%;
+        right: -50%;
+        transform: translateY(-50%);
+        font-size: var(--font-size-medium);
+    }
+`;
 
-const DateValue = styled.span``;
+const Start = styled.div`
+    margin: var(--margin-medium) 0;
+`;
+
+const End = styled(Start)``;
 
 export default function Progress() {
+    const completeCount = useRecoilValue(completeCountState);
+    const completePercent = useRecoilValue(completePercentState);
+
+    const [isDate, setIsDate] = useRecoilState(dateSubmitted);
+    const [startDate, setStartDate] = useRecoilState(startDateState);
+    const endDate = useRecoilValue(endDateState);
+    const { register, handleSubmit } = useForm();
+    const getStartDate = (data) => {
+        setStartDate(data.date);
+        setIsDate(true);
+    };
+
     return (
         <ProgressWrapper>
-            <Per>20%</Per>
-            <CompleteCount>0/66</CompleteCount>
+            <Per>{completePercent}%</Per>
+            <CompleteCount>{completeCount}/66</CompleteCount>
             <ProgressBarBack>
-                <ProgressBar />
+                <ProgressBar per={completePercent} />
             </ProgressBarBack>
-            <ProgressBottom>
-                <FailCount>0/3</FailCount>
-                <DateValue>2023/09/31 - 2024/01/01</DateValue>
-            </ProgressBottom>
+            <DateValue>
+                {isDate === false ? (
+                    <form onSubmit={handleSubmit(getStartDate)}>
+                        <label>시작일 : </label>
+                        <input
+                            type="date"
+                            {...register('date', { required: true })}
+                        />
+                        <button>
+                            <FaPlus />
+                        </button>
+                    </form>
+                ) : null}
+
+                {isDate ? (
+                    <StartEnd>
+                        <Start>
+                            <span>시작일 : </span>
+                            <span>{startDate}</span>
+                        </Start>
+                        <End>
+                            <span>종료일 : </span>
+                            <span>{endDate}</span>
+                        </End>
+                        <button
+                            onClick={() => {
+                                setIsDate(false);
+                            }}
+                        >
+                            <SlNote />
+                        </button>
+                    </StartEnd>
+                ) : null}
+            </DateValue>
         </ProgressWrapper>
     );
 }
--- src/atom.js
+++ src/atom.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
@@ -27,6 +27,20 @@ export const completeState = atom({
     effects_UNSTABLE: [persistAtom],
 });
 
+export const completeCountState = atom({
+    key: 'complete_count',
+    default: 60,
+    effects_UNSTABLE: [persistAtom],
+});
+
+export const completePercentState = selector({
+    key: 'complete_percent',
+    get: ({ get }) => {
+        const complete_count = get(completeCountState);
+        return Math.floor((complete_count / 66) * 100);
+    },
+});
+
 export const failSubmitted = atom({
     key: 'fail_submitted',
     default: false,
@@ -36,5 +50,41 @@ export const failSubmitted = atom({
 export const failState = atom({
     key: 'fail_state',
     default: '',
+    effects_UNSTABLE: [persistAtom],
+});
+
+// Date
+
+export const dateSubmitted = atom({
+    key: 'date_submitted',
+    default: false,
+    effects_UNSTABLE: [persistAtom],
+});
+
+export const startDateState = atom({
+    key: 'start_date',
+    default: '',
+    effects_UNSTABLE: [persistAtom],
+});
+
+export const endDateState = selector({
+    key: 'end_date',
+    get: ({ get }) => {
+        const start_date = get(startDateState);
+        if (start_date !== '') {
+            const inputDate = new Date(start_date);
+            // 입력된 날짜에 일수를 더한 새로운 Date 객체 생성
+            const newDate = new Date(inputDate);
+            newDate.setDate(inputDate.getDate() + 66);
+            // 새로운 날짜를 원하는 형식으로 포맷
+            const year = newDate.getFullYear();
+            const month = (newDate.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 2자리로 포맷
+            const day = newDate.getDate().toString().padStart(2, '0'); // 일도 2자리로 포맷
+
+            const resultDateStr = `${year}-${month}-${day}`;
+            return resultDateStr;
+        }
+        return;
+    },
     effects_UNSTABLE: [persistAtom],
 });
