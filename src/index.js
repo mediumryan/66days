1c5b55f4598784118bd33e43dfbfcb7637419fe4
Ryan <straightwind0726@gmail.com>
2024-02-06 17:16:52 +0900

    [Update] Add to the logic for going scroll to top when pathname has changed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './CSS/index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
+// import css
+import './CSS/index.css';
+// import components
+import ScrollToTop from './helper/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RecoilRoot>
             <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <ScrollToTop />
                 <App />
             </BrowserRouter>
         </RecoilRoot>
